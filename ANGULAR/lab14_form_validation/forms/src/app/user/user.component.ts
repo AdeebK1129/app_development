a1003e7f22336ce137be0b7b0bc2d57b5262f636
Adeeb <adeeb19909@gmail.com>
2024-08-29 15:39:53 -0400

    Finished lab 14

--- ANGULAR/lab14_form_validation/forms/src/app/user/user.component.ts
+++ ANGULAR/lab14_form_validation/forms/src/app/user/user.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { User } from './user.model';
 import { User_Repository } from './repository.model';
-import { FormsModule } from '@angular/forms' // to work with Angular Directive *ng
+import { FormsModule, NgForm } from '@angular/forms' // to work with Angular Directive *ng
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -14,6 +14,10 @@ import { CommonModule } from '@angular/common';
 export class UserComponent {
   model:User_Repository = new User_Repository()
   newuser:User = new User()
+  parsedJson:any
+  parsedJsonName:any
+  parsedJsonEmail:any
+  usercounter:number = this.model.getUser().length
 
   get jsonUser(){
     return JSON.stringify(this.newuser)
@@ -21,9 +25,29 @@ export class UserComponent {
 
   addUser(u:User){
     console.log("New User is " + this.jsonUser)
+    this.usercounter += 1
+    this.model.addUser(new User(this.usercounter, this.parsedJsonName, this.parsedJsonEmail))
   }
 
   displayLog(model:any){
     console.log("New User Model", model)
   }
+
+  // formSubmit property to set the validation rule
+  formSubmit:boolean = false
+
+  // submitForm() method to handle the form data on submission
+  submitForm(form:NgForm){
+    this.formSubmit = true
+    if(form.valid){
+      this.parsedJson = JSON.parse(JSON.stringify(this.newuser))
+      this.parsedJsonName = this.parsedJson.name
+      this.parsedJsonEmail = this.parsedJson.email
+      this.addUser(this.newuser)
+      this.newuser = new User()
+      form.reset()
+      this.formSubmit = false
+    }
+  }
+
 }
