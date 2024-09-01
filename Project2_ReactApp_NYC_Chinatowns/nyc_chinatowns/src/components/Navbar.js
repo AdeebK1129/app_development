002433e3279cca4d41bdc554f8b1d559c6d03159
Adeeb <adeeb19909@gmail.com>
2024-09-01 12:36:11 -0400

    added use state hook to set active navbar color

--- Project2_ReactApp_NYC_Chinatowns/nyc_chinatowns/src/components/Navbar.js
+++ Project2_ReactApp_NYC_Chinatowns/nyc_chinatowns/src/components/Navbar.js
@@ -1,22 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../assets/logo.png";
 
 function Navbar() {
+  const [activePage, setActivePage] = useState('/');
+
   return (
     <div className="navbar">
       <div className="logo">
-        <Link to="/">
+        <Link to="/" className={activePage === '/' ? 'active' : ''} onClick={() => setActivePage('/')}>
           <img src={logo} alt="Logo" />
           <span>Chinatowns of NYC</span>
         </Link>
       </div>
       <div>
-        <Link to="/">Home</Link>
-        <Link to="/flushing">Flushing Chinatown</Link>
-        <Link to="/brooklyn">Brooklyn Chinatown</Link>
-        <Link to="/manhattan">Manhattan Chinatown</Link>
+        <Link 
+          to="/" 
+          className={activePage === '/' ? 'active' : ''} 
+          onClick={() => setActivePage('/')}
+        >
+          Home
+        </Link>
+        <Link 
+          to="/flushing" 
+          className={activePage === '/flushing' ? 'active' : ''} 
+          onClick={() => setActivePage('/flushing')}
+        >
+          Flushing Chinatown
+        </Link>
+        <Link 
+          to="/brooklyn" 
+          className={activePage === '/brooklyn' ? 'active' : ''} 
+          onClick={() => setActivePage('/brooklyn')}
+        >
+          Brooklyn Chinatown
+        </Link>
+        <Link 
+          to="/manhattan" 
+          className={activePage === '/manhattan' ? 'active' : ''} 
+          onClick={() => setActivePage('/manhattan')}
+        >
+          Manhattan Chinatown
+        </Link>
       </div>
     </div>
   );
